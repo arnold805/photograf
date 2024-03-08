90a1933035bfcaf4c8f40e0b174b29c7c8f1f6d9
Greg <garnold.805@gmail.com>
2024-03-08 17:56:57 -0500

    removes app.spec.ts and adds some more basic tests in pages.spec.ts

--- e2e/pages.spec.ts
+++ e2e/pages.spec.ts
@@ -1,14 +1,22 @@
 import { test, expect } from "@playwright/test";
 
-test("should navigate to the about page", async ({ page }) => {
+test("should navigate to the home page", async ({ page }) => {
   // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
   await page.goto("/home");
   // Find an element with the text 'About Page' and click on it
-  await page.getByText("About Page").click();
+  await page.getByText("Welcome to Photograf").click();
   // The new url should be "/about" (baseURL is used there)
-  await expect(page).toHaveURL("/home/about");
+});
+test("should navigate to the login page", async ({ page }) => {
+  // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
+  await page.goto("/home");
+  // Find an element with the text 'About Page' and click on it
+  await page.getByText("Login Page").click();
+  // The new url should be "/about" (baseURL is used there)
+  await expect(page).toHaveURL("/home/login");
   // The new page should contain an h1 with "About Page"
   await expect(page.getByRole("heading", { level: 1 })).toContainText(
-    "About Page",
+    "Login Page",
   );
 });
+
--- pages/home/login.tsx
+++ pages/home/login.tsx
@@ -1,11 +1,11 @@
 import styles from "../../styles/Home.module.css";
 import Link from "next/link";
 
-export default function About() {
+export default function Login() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1>About Page</h1>
+        <h1>Login Page</h1>
         <p className={styles.description}>
           <Link href="/home">&larr; Go Back</Link>
         </p>
